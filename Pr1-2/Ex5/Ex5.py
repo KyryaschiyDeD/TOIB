ac9709bda8c674c0e363e3b1a6bae43a41dae02b
Kyraschiy_DeD <mic.makarov@mail.ru>
2023-09-28 21:59:57 +0300

    Добавлен №5

--- Pr1-2/Ex5/Ex5.py
+++ Pr1-2/Ex5/Ex5.py
@@ -1,34 +1,71 @@
 import requests
 import hashlib
+import string
+import random
 
+def generate_password(length = 8):
+	chars = string.ascii_letters + string.digits + string.punctuation
+	password = ''.join(random.choice(chars) for i in range(length))
+	return password
 
-def openFile():
-    filename = input('Enter filename (default password.txt): ')
-
+def openFile(filename):
     if filename:
-        print ('Opening :' + filename + '...')
+        print ('Opening : ' + filename + '...')
     else:
         print('using password.txt')
         filename = 'password.txt'
    
-    fromFile = ""
+    linesFromFile = ""
     with open(filename, 'r', encoding='utf-8') as file:
         for passwFromFile in file:
-                fromFile = goodPass + passwFromFile
-    checkPass(fromFile)
+            linesFromFile = linesFromFile + passwFromFile
+            
+    checkPass(linesFromFile)
 
 
 def checkPass(passFromFile):
-    for line in passFromFile:
+    for line in passFromFile.splitlines():
         sha1_hash = hashlib.sha1((line.strip()[line.strip().find(',')+1:]).encode())
         sha1_hex = sha1_hash.hexdigest()
         req = requests.get('https://api.pwnedpasswords.com/range/'+sha1_hex[:5])
         reqStr = str(req.content.decode('ascii')).replace('\r','')
         if reqStr.find(sha1_hex[5:].upper()) == -1:
-            print("The user's: " + (line.strip()[:line.strip().find(',')]) + " password has not been stolen \n")
+            print("The user's: " + (line.strip()[:line.strip().find(',')]) + " password has not been stolen! GOOD GOOD GOOD! \n ")
         else:
-            print("The user's: " + (line.strip()[:line.strip().find(',')]) + " password has been stolen!")
+            print("The user's: " + (line.strip()[:line.strip().find(',')]) + " password has been stolen! BAD BAD BAD!!!!!")
 
+
+def generateFile():
+    countFromConsole = input('Enter users count: ')
+    if countFromConsole:
+         if int(countFromConsole):
+              count = int(countFromConsole)
+         else:
+              print('Error')
+              return
+    else:
+         print('Error')
+         return
+         
+    lenghtFromConsole = input('Enter password length (default 8): ')
+
+    if lenghtFromConsole:
+        if int(lenghtFromConsole):
+            lenght = int(lenghtFromConsole)
+        else:
+            print('Error (...we are use default...)')     
+    
+    if (not lenght):
+        lenght = 8
+           
+    filename = "password.txt"
+    file = open(filename, "w+")
+    number = 0
+    for _ in range(count):
+        file.write("user" + str(number) + "," + generate_password(lenght)+"\n")
+        number = number + 1
+    file.close
+    return filename
 # ------------------------------------------------------
 strInput = ""
 while (strInput != "E"):
@@ -38,9 +75,9 @@ while (strInput != "E"):
     strInput = input(": ")
     match strInput.split():
         case ["O"]:  
-            openFile()
+            openFile(input('Enter filename (default password.txt): '))
         case ["G"]:  
-            generateFile()
+            openFile(generateFile())
         case ["E"]:  
             break;
         case _:
